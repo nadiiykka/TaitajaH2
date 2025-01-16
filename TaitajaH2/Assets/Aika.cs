c3d96e10d7f30e77a2b6c35c1b4fd48736a2cec9
IslaIsopahkala <isla.isopahkala@vuoksi.fi>
2025-01-16 11:48:50 +0200

    sdjdfosdf

---
+++ TaitajaH2/Assets/Aika.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Aika : MonoBehaviour
+{
+    public float timeRemanining = 0;
+    public bool timeIsRunning = false;
+    public TMP_Text timeText;
+    void Start()
+    {
+        timeIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timeIsRunning)
+        {
+            if (timeRemanining >= 0)
+            {
+                timeRemanining += Time.deltaTime;
+                DisplayTime(timeRemanining);
+            }
+
+            if (timeRemanining >= 59)
+            {
+                timeIsRunning = false;
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timeText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+    }
+}
